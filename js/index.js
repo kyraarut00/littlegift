e35be7348e0dbc7c4725474db3006536ee486e30
kyraarut00 <150677153+kyraarut00@users.noreply.github.com>
2025-01-15 22:30:24 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Hi pretty, Alana.'.split('')
+const text = 'Hi pretty, Anggit.'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
